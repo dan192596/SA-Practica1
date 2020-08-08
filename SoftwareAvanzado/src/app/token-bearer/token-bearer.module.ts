8610a9159fef92ad26a5afe9937888473858a49e
Joel Daniel Perez Camey <dperez@yetis.io>
2020-08-08 10:00:28 -0600

    Creacion carpetas

---
+++ SoftwareAvanzado/src/app/token-bearer/token-bearer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TokenBearerRoutingModule } from './token-bearer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TokenBearerRoutingModule
+  ]
+})
+export class TokenBearerModule { }
